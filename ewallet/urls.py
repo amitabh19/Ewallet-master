f81134effa3fd01db303da5c6d809e0f4a675105
amitabh19 <amitabh19101998@gmail.com>
2019-02-05 19:24:19 +0530

    database, signup and login working as well as showing errors

--- ewallet/urls.py
+++ ewallet/urls.py
@@ -15,10 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.contrib.auth import views
+from wallet import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('signup/', include('wallet.urls')),
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
 
 ]
--- wallet/views.py
+++ wallet/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.contrib.auth import login, authenticate
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth.forms import AuthenticationForm
+
 from django.shortcuts import render, redirect
 from .forms import SignUpForm, UserCreationForm, ProfileForm
 from django.contrib.messages import constants as messages
@@ -10,24 +12,6 @@ from django.db import connection
 
 def index(request):
     return HttpResponse("namaste beta")
-
-'''
-def signup(request):
-    if request.method == 'POST':
-        form = SignUpForm(request.POST)
-        profile_form = ProfileForm(request.POST)
-        if form.is_valid() and profile_form.is_valid():
-            user = form.save() and profile_form.save()
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(email=user.email, password=raw_password)
-            login(request, user)
-            return HttpResponse("namaste beta")
-    else:
-        form = SignUpForm()
-        profile_form = ProfileForm()
-    return render(request, 'wallet/signup.html', {'form': form, 'profile_form': profile_form},
-                  )
-'''
 
 
 def signup(request):
@@ -42,10 +26,10 @@ def signup(request):
             p = UserProfile.objects.get(user_id=u.pk)
             p.mob = mobile
             p.save()
+            return HttpResponseRedirect(reverse('login'))
+    # else:
 
-            return HttpResponse('yo yo yo')
-        else:
-            return HttpResponse('no no no')
+
     else:
         form = SignUpForm()
         profile_form = ProfileForm()
@@ -54,3 +38,13 @@ def signup(request):
         'profile_form': profile_form
     })
 
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            return HttpResponse('Successfully logged in')
+
+    else:
+        form = AuthenticationForm()
+    return render(request, 'wallet/login.html', {'form': form})
